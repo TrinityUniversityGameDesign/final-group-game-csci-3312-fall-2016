dbd15a3916c35652c56d9d637455217729760c32
Marcus Whitten <cwhitte1@trinity.edu>
2016-10-29 12:50:36 -0500

    RandonInputScript

---
+++ Assets/Scripts/RandomInputGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomInputGenerator : MonoBehaviour {
+
+	public enum Buttons {A,B,Y,X};
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
