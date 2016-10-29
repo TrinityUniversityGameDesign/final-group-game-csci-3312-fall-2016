379526e53877f3a29a98b974f1efe2d77fe7d6d9
zzimdars <zzimdars@trinity.edu>
2016-10-29 12:51:46 -0500

    UI Text Display

---
+++ Assets/Scripts/MemoryDisplay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MemoryDisplay : MonoBehaviour {
+
+    public Text RandomInputText;
+    int x = 0;
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        RandomInputText.text = x.ToString();
+        timeToDisplay();
+	}
+
+    void timeToDisplay()
+    {
+        x += 1;
+    }
+}
