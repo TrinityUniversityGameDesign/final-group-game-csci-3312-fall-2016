1a74b00b2be27210e381e4493c6eca13032e37ce
boakes <boakes1@trinity.local>
2016-10-29 14:23:12 -0500

    PlayerStuff Added

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,59 +2,155 @@
 using System.Collections;
 using System.Collections.Generic;
 
+public class PlayerInput {
+	private string p_hor;
+	private string p_vert;
+	private string p_ABut;
+	private string p_BBut;
+	private string p_YBut;
+	private string p_XBut;
+	private string p_startBut;
+	private string p_backBut;
+	private string p_L3; 
+	private string p_R3; 
+	private string p_LB; 
+	private string p_RB; 
+	private string p_LT; 
+	private string p_RT; 
+	private string p_rightX;
+	private string p_rightY;
+	private string p_DPadX;
+	private string p_DPadY;
 
+	public string hor{
+		get { return p_hor; }
+		set { p_hor = value; }
+	}
+	public string vert{
+		get { return p_vert; }
+		set { p_vert = value; }
+	}
+	public string ABut{
+		get { return p_ABut; }
+		set { p_ABut = value; }
+	}
+	public string BBut{
+		get { return p_BBut; }
+		set { p_BBut = value; }
+	}
+	public string YBut{
+		get { return p_YBut; }
+		set { p_YBut = value; }
+	}
+	public string XBut{
+		get { return p_XBut; }
+		set { p_XBut = value; }
+	}
+	public string startBut{
+		get { return p_startBut; }
+		set { p_startBut = value; }
+	}
+	public string backBut{
+		get { return p_backBut; }
+		set { p_backBut = value; }
+	}
+	public string L3{
+		get { return p_L3; }
+		set { p_L3 = value; }
+	}
+	public string R3{
+		get { return p_R3; }
+		set { p_R3 = value; }
+	}
+	public string LB{
+		get { return p_LB; }
+		set { p_LB = value; }
+	}
+	public string RB{
+		get { return p_RB; }
+		set { p_RB = value; }
+	}
+	public string LT{
+		get { return p_LT; }
+		set { p_LT = value; }
+	}
+	public string RT{
+		get { return p_RT; }
+		set { p_RT = value; }
+	}
+	public string rightX{
+		get { return p_rightX; }
+		set { p_rightX = value; }
+	}
+	public string rightY{
+		get { return p_rightY; }
+		set { p_rightY = value; }
+	}
+	public string DPadX{
+		get { return p_DPadX; }
+		set { p_DPadX = value; }
+	}
+	public string DPadY{
+		get { return p_DPadY; }
+		set { p_DPadY = value; }
+	}
+}
 
 public class PlayerScript : MonoBehaviour {
+	public string playerNum;
+	public PlayerInput iC;
 
-	public string hor;
-	public string vert;
-	public string ABut;
-	public string BBut;
-	public string YBut;
-	public string XBut;
-	public string startBut;
-	public string backBut;
-	public string L3; 
-	public string R3; 
-	public string LB; 
-	public string RB; 
-	public string LT; 
-	public string RT; 
-	public string rightX;
-	public string rightY;
-	public string DPadX;
-	public string DPadY;
-	public int playerNum;
+	public GameObject memDisp; 
 
 
 
-	public enum Buttons {A,B,X,Y};
-	List<Buttons> InputList;
+	public List<MemoryDisplay.Buttons> InputList;
 	// Use this for initialization
+
+	void Awake(){
+		iC = new PlayerInput ();
+		InputList = new List<MemoryDisplay.Buttons> ();
+	}
+
 	void Start () {
-		playerNum = 1; // should change on instantiation 
-		hor = "Horizontal_" + playerNum;
-		vert = "Vertical_" + playerNum;
-		ABut = "A_" + playerNum;
-		BBut = "B_" + playerNum;
-		YBut = "Y_" + playerNum;
-		XBut = "X_" + playerNum;
-		startBut = "Start_" + playerNum;
-		backBut = "Back_" + playerNum;
-		L3 = "L3_" + playerNum; 
-		R3 = "R3_" + playerNum; 
-		LB = "LB_" + playerNum; 
-		RB = "RB_" + playerNum; 
-		LT = "LT_" + playerNum; 
-		RT = "RT_" + playerNum; 
-		rightX = "RightX_" + playerNum;
-		rightY = "RightY_" + playerNum;
-		DPadX = "DPadX_" + playerNum; 
-		DPadY = "DPadY_" + playerNum;
+		memDisp = GameObject.Find ("MemoryDisplay");
+		playerNum = "P1"; // should change on instantiation
+		iC.hor = "Horizontal_" + playerNum;
+		iC.vert = "Vertical_" + playerNum;
+		iC.ABut = "A_" + playerNum;
+		iC.BBut = "B_" + playerNum;
+		iC.YBut = "Y_" + playerNum;
+		iC.XBut = "X_" + playerNum;
+		iC.startBut = "Start_" + playerNum;
+		iC.backBut = "Back_" + playerNum;
+		iC.L3 = "L3_" + playerNum; 
+		iC.R3 = "R3_" + playerNum; 
+		iC.LB = "LB_" + playerNum; 
+		iC.RB = "RB_" + playerNum; 
+		iC.LT = "LT_" + playerNum; 
+		iC.RT = "RT_" + playerNum; 
+		iC.rightX = "RightX_" + playerNum;
+		iC.rightY = "RightY_" + playerNum;
+		iC.DPadX = "DPadX_" + playerNum; 
+		iC.DPadY = "DPadY_" + playerNum;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		if (Input.GetButtonDown (iC.ABut)) {
+			InputList.Add (MemoryDisplay.Buttons.A);
+		}
+		if (Input.GetButtonDown (iC.BBut)) {
+			InputList.Add (MemoryDisplay.Buttons.B);
+		}
+		if (Input.GetButtonDown (iC.YBut)) {
+			InputList.Add (MemoryDisplay.Buttons.Y);
+		}
+		if (Input.GetButtonDown (iC.XBut)) {
+			InputList.Add (MemoryDisplay.Buttons.X);
+		}
+
+
 	}
 }
