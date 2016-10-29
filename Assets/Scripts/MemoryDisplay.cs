835771986b6ab70166b940e7bc0bf4d0795239df
zzimdars <zzimdars@trinity.edu>
2016-10-29 14:07:12 -0500

    Added Marcus' Random function to the UI Display

--- Assets/Scripts/MemoryDisplay.cs
+++ Assets/Scripts/MemoryDisplay.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 
+public enum Buttons { A, B, X, Y }; // buttons that can be recognized
+
 public class MemoryDisplay : MonoBehaviour {
 
-	public enum Buttons {A,B,X,Y}; // buttons that can be recognized
+	
 	List<Buttons> InputList; // list of inputs that will be memorized by player *remember to clear after every round
 	public int Round = 0; // records how many 'rounds' have occured during the game
+    float displayTime = 2f;
 
     public Text RandomInputText;
     int x = 0; // temp display number
 	// Use this for initialization
 	void Start () {
-       
-	}
+        getRandomInputs();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -27,8 +30,45 @@ public class MemoryDisplay : MonoBehaviour {
         RandomInputText.text = message;
         RandomInputText.enabled = true;
         yield return new WaitForSeconds(delay);
-        RandomInputText.enabled = false;
+        //RandomInputText.enabled = false;
         x += 1;
+    }
+
+    //gets inputs from generateRandomInputs
+    //compares inputs to create message and call timeToDisplay
+    void getRandomInputs()
+    {
+        string message = "";
+
+        generateRandomInputs(5);
+
+        for (int i = 0; i < InputList.Count; i++)
+        {
+            Buttons tmp = InputList[i];
+
+            switch (tmp)
+            {
+                case Buttons.A:
+                    message += "A ";
+                    break;
+                case Buttons.B:
+                    message += "B ";
+                    break;
+                case Buttons.X:
+                    message += "X ";
+                    break;
+                case Buttons.Y:
+                    message += "Y ";
+                    break;
+            }
+        }
+
+        //for(int i = 0; i < InputList.Count; i++)
+        //{
+        //    Debug.Log(InputList[i] + i.ToString());
+        //}
+        //Debug.Log(message);
+        StartCoroutine(timeToDisplay(message, displayTime));
     }
 
     void generateRandomInputs(int roundNumber) {
