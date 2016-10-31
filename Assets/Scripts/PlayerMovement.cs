2b1ef4a72ef804c6220d00f5fb4cc4f71004c26a
Jeffrey Darrah <jdarrah@trinity.local>
2016-10-30 22:16:56 -0500

    yeah thats a good branch
    got movement physics. need bumper phyiscs

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    public float moveSpeed = 15f;
+    public float speedCap = 0.89f;
+    public string horizontalCtrl = "Horizontal_P1";
+    public string verticalCtrl = "Vertical_P1";
+
+    //private Vector3 startPos;
+
+    private Rigidbody2D theRigidBody;
+    
+	// Use this for initialization
+	void Start () {
+        theRigidBody = GetComponent<Rigidbody2D>();
+        //startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float inputX = Input.GetAxis(horizontalCtrl);
+        float inputY = Input.GetAxis(verticalCtrl);
+
+        theRigidBody.AddForce(new Vector2(inputX * moveSpeed, inputY * moveSpeed));
+        if(theRigidBody.velocity.magnitude > speedCap)
+        {
+            theRigidBody.velocity.Normalize();
+            theRigidBody.velocity = theRigidBody.velocity * speedCap; 
+        }
+    }
+}
