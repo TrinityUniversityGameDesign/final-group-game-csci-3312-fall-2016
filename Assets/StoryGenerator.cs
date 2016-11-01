ad3c988b0eda1eef95a55e82e4b10162f4c9cdb5
Andy Leeds <aleeds@trinity.edu>
2016-11-01 17:01:20 -0500

    Character Support
    The story generation now supports dynamic main character creation. It
    DOES not have any randomness yet.

--- Assets/StoryGenerator.cs
+++ Assets/StoryGenerator.cs
@@ -5,8 +5,29 @@ public class StoryGenerator : MonoBehaviour {
 
     class MainCharacter
     {
-
-    }
+        public string adjective;
+        public string occupation;
+        public string name;
+        public string history;
+        public string his;
+        public string he;
+        public string him;
+        public string servant;
+        public string hobby;
+        public MainCharacter(string adj, string occ, string nm, string hist, string his_t,
+                             string he_t, string him_t, string servant_t, string hobby_t)
+        {
+            adjective = adj;
+            occupation = occ;
+            name = nm;
+            history = hist;
+            his = his_t;
+            he = he_t;
+            him = him_t;
+            servant = servant_t;
+            hobby = hobby_t;
+        }
+    } 
 
     /* The branching story:
      * Advanture can lead to being: (generate_opening)
@@ -18,77 +39,143 @@ public class StoryGenerator : MonoBehaviour {
      *         You are killed, and the treasure is taken. By your crew. (generate_killed)
      *         Or, they leave you stranded to die somehwere. (generate_stranded)
      *             In Either case, the crew utlimately kill each other, leaving the treasure somewhere along the route home. Which leads to the end state. (generate_lost).
-     *     In in the end, legends of this lost treasure have trickled around the world. (generate_legends).
     */     
 
     // Eventually need to move out of here into a file as a template (do I?).
     // The things in {} can be swapped out for similar words. 
     // When making it, have it so corresponding words still appear, etc.
-    string generate_opening()
+    string generate_opening(MainCharacter mc)
     {
 
-        string s = "The great {absurdist} {explorer} {Schwifty Von Amrack} was noted for discovering the {staff} of {Alluhaasds} at the {great} {temple} of {Ushjdi}.";
-        s += " However, {his} {forgotten} {discovery} was the discovery of the {pubescent} {medalion} of {Amadusos}, which {he} found after {winning} at the {Great} {Games} {at Osismosos}.";
-        s += "This is not the tale of that discovery, but of what befell {him} after...";
+        string s = "The great " + mc.adjective + " " + mc.occupation + " " +  mc.name +  " was noted for discovering the " + mc.history;
+        s += " However, " + mc.his + " forgotten discovery was the discovery of the {pubescent} {medalion} of {Amadusos}, which " + mc.he + " found after {winning} at the {Great} {Games} {at Osismosos}.";
+        s += "This is not the tale of that discovery, but of what befell " + mc.him + " after... ";
         return s;
     }
 
-    string generate_cursed()
+    string generate_cursed(MainCharacter mc)
     {
-        string s = "However, there were {terrible} tales told of that {pubescent} {medalion}. Tales of a {horrible} {curse}, which befalls any who {fuck} that {pubescent} item.";
+        string s = "However, there were {terrible} tales told of that {pubescent} {medalion}. Tales of a {horrible} {curse}, which befalls any who are near that {pubescent} item.";
         s += "The stories told of being {burned alive} and being {gutted like a pig}, and {several} spoke of being {eternally ostricized by everyone who had pretended to love} you. ";
-        s += "It did not initially inflict {Schwifty Von Amrack}, it instead hit {his} trusty {bear-man servant}. He was tragically {eternally ostricized by everyone who had pretended to love} {him}.";
-        s += "Leaving {Schwifty Von Amrack} devastated. The curse ravaged the crew, until only the {absurdist} {explorer} {Schwifty Von Amrack} was left alone.";
+        s += "It did not initially inflict " + mc.name + " , it instead hit " + mc.his + " trusty " + mc.servant + ", who was tragically {eternally ostricized by everyone who had pretended to love} ";
+        s += mc.him + "." + " Leaving " + mc.name + " devastated. The curse ravaged the crew, until only the " + mc.adjective + " " + mc.occupation + " " + mc.name + " was left alone. ";
         return s;
     }
 
 
-    string generate_old() {
+    string generate_old(MainCharacter mc) {
     
-        string s = "Realizing the gravity of his {doom}, the {aburdist} {explorer} abandoned the {medalion}. He {prayed} that whatever {demonic} {gods} which {cursed} {Amadusos}'s {pubescent}";
-        s += "{medalion}. He abandoned the {medalion}, and returned home to {the Kingslandistanian Empire}.";
+        string s = "Realizing the gravity of " + mc.his + " {doom}, the " + mc.adjective + " " + mc.occupation + " abandoned the {medalion}.";
+        s += mc.name + " {prayed} that whatever {demonic} {gods} which {cursed} {Amadusos}'s {pubescent}";
+        s += "{medalion}. The " + mc.occupation + " abandoned the {medalion}, and returned home.";
+
         return s;
     }
 
-    string generate_horrible()
+    string generate_horrible(MainCharacter mc)
     {
-        string s = "Tragically, like it was too late for the crew, it was too late for that {aburdist} {Schwifty Von Amrack}, even after {he} had abandoned the {medalion}.";
-        s += "The famous {explorer} was {cursed} by a fate more horrible than any of the fates spoken of in the tales.";
-        s += "{Schwifty Von Amrack was {brutally} {drowned alive and resurrected before being eaten alive} by {minions} of {Amadusos} {him}self.";
+        string s = "Tragically, like it was too late for the crew, it was too late for that " + mc.adjective + " " +  mc.name + " , even after " + mc.he + " had abandoned the {medalion}.";
+        s += "The famous " + mc.occupation + " was {cursed} by a fate more horrible than any of the fates spoken of in the tales.";
+        s += mc.name + " was {brutally} {drowned alive and resurrected before being eaten alive} by {minions} of {Amadusos} {him}self. ";
         return s;
     }
 
     string generate_abandoned()
     {
-        string s = "The {pubescent} {medalion} was left in one of {Amadusos} {lesser} {temples}, in a truly desperate attempt to prevent the {curse}. It remains there to this day.";
+        string s = "The {pubescent} {medalion} was left in {the title of the game}, in a truly desperate attempt to prevent the {curse}. It remains there to this day. ";
         return s;
     }
 
-    // All 'generate' below are incomplete.
-    string generate_scorned()
+
+    string generate_scorned(MainCharacter mc)
     {
-        return "You died old man!";
+        string s = "The {medalion} was valued at many {millions} of dollars, enough for each of the crew of {15} to live rich, for the rest of their lives." +  mc.name + " ";
+        s += "was a cruel leader, causing the crew to dislike " + mc.him + " greatly. The crew felt scorned by the " + mc.adjective + " " + mc.occupation;
+        s += ", who had always {beat them} {visciously}. They turned on " + mc.him + ".";
+        s += "During " + mc.name + "'s daily " + mc.hobby + ", they surprised " + mc.him + ", {putting him in a hole in the ground}. They left to debate " +  mc.his + " fate. ";
+        return s;
     }
 
-    string generate_killed()
+    string generate_killed(MainCharacter mc)
     {
-        return "You died old man!";
+        string s = "Of all of the crew, only " + mc.his + " trusty " + mc.servant + " defended" +  mc.him + ", citing " + mc.his + " {generosiy} despite how " + mc.he + "{beat them} {visciously}.";
+        s += "Each of the crew came up with a terrible fate for the {explorer}. One suggested " + mc.he + " be {fed to wild dogs}. Another that " + mc.he + " be {decapitated}.";
+        s += "Ultimately they settled that " + mc.he + " should be {buried alive beneath the sand}. Each crew member one by one spat on " + mc.him + " , as " + mc.he + " was ";
+        s += "{ buried alive beneath the sand}. ";
+        return s;
     }
 
-    string generate_stranded()
+    string generate_stranded(MainCharacter mc)
     {
-        return "You died old man!";
+        string s = "The crew pondered " +  mc.his + " fate. " + mc.name + "'s long trusted " +  mc.name + " argued passionately that " + mc.he + " be killed, citing years of abuse. The others ";
+        s += "had only seen the kind side of the " + mc.occupation + ", and noted that, though {harsh}, was always {fair}. They decided that although " + mc.name + " did not deserve the treasure which ";
+        s += "they had worked so hard to discover, + " + mc.he + " did not deserve death. Ultimately, the crew left" + mc.him + " behind, {on} a {deserted island}. ";
+        s += "As a final show of hatred,  " + mc.name + "'s trusted ";
+        s += mc.servant +  " {spat} on " +  mc.him + ". " + mc.name + " was never seen again.";
+        return s;
+    }
+    // Below here the character stuff isn't done.
+    string generate_lost(MainCharacter mc)
+    {
+        string s = "Much like how the crew turned on " +  mc.name  + ", they turned on each other. " +  mc.name + "'s trusted " + mc.servant + " was the first of the crew to be killed by";
+        s += " the others. Each crew died one by one, as they each betrayed eachother. The final crew member {starved} to death. The {medalion} was lost in the {sands of the desert Calypso} near ";
+        s += " the {Title of the Game}. It was eventually taken to the {title of the game}.";
+        return s;
     }
 
-    string generate_lost()
+    string build_history()
     {
-        return "You died old man!";
+        return "staff of Alluhaasds at the great temple of Ushjdi";
     }
 
-    string generate_legends()
+    string build_servant()
     {
-        return "You died old man!";
+        return "man-bear servant";
     }
+
+    MainCharacter build_main_character()
+    {
+        // The history needs to be autogenerated too.
+        return new MainCharacter("absurdist", "explorer", "Schwifty Von Amrack", build_history(), "his",
+                                 "he", "him", build_servant(), "chess session");
+
+    }
+
+    string[] make_story()
+    {
+        string[] story_pieces = new string[4];
+        MainCharacter mc = build_main_character();
+        story_pieces[0] = generate_opening(mc);
+        bool cursed = false;
+        if (cursed)
+        {
+            story_pieces[1] = generate_cursed(mc);
+            bool old = false;
+            if (old)
+            {
+                story_pieces[2] = generate_old(mc);
+            } else
+            {
+                story_pieces[2] = generate_horrible(mc);
+            }
+            story_pieces[3] = generate_abandoned();
+        } else
+        {
+            story_pieces[1] = generate_scorned(mc);
+            bool killed = false;
+            if (killed)
+            {
+                story_pieces[2] = generate_killed(mc);
+            }
+            else
+            {
+                story_pieces[2] = generate_stranded(mc);
+            }
+            story_pieces[3] = generate_lost(mc);
+        }
+        return story_pieces;
+    }
+
     // Use this for initialization
     void Start () {
 	
