ed508d0893b1c13138ddf1cfa75e5fdd86b943c5
chayes4 <chayes@trinity.edu>
2016-11-01 16:32:56 -0500

    Added Prefabs for player and trivia controller

---
+++ Assets/Scripts/TriviaPlayer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TriviaPlayer : MonoBehaviour
+{
+	public Text txtScore;
+	public int score = 0;
+	public int playerNumber;
+	bool answered;
+	public GameObject controllerGameObject;
+	TrivaController controller;
+
+	// Use this for initialization
+	void Start ()
+	{
+		answered = false;
+		controller = controllerGameObject.GetComponent<TrivaController> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		txtScore.text = "Score : " + score;
+		if (!answered) {
+			
+			if (Input.GetAxis ("A_P" + 1) > 0) {
+				answered = true;
+				score += controller.amIRight ('a');
+			} else if (Input.GetAxis ("X_P" + 1) > 0) {
+				answered = true;
+				score += controller.amIRight ('x');
+			}
+			else if (Input.GetAxis ("B_P" + 1) > 0) {
+				answered = true;
+				score += controller.amIRight ('b');
+			}
+			else if (Input.GetAxis ("Y_P" + 1) > 0) {
+				answered = true;
+				score += controller.amIRight ('y');
+			}
+		}
+	}
+}
