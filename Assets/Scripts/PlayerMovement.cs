eb65cdec2c33f04da3553cdd0a4b33e2913bc61c
craig-burton <cburton@trinity.edu>
2016-11-01 16:48:14 -0500

    Added player prefab and player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public int playerNum;
+	public float walkSpeed;
+
+	private Rigidbody2D rigid;
+	private bool canDash;
+
+
+	private string vertAxis;
+	private string horAxis;
+	private string jumpButton;
+	private string actionButton;
+
+
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody2D> ();
+		vertAxis = "VerticalP" + playerNum.ToString();
+		horAxis = "HorizontalP" + playerNum.ToString();
+		jumpButton = "JumpP" + playerNum.ToString();
+		actionButton = "ActionP" + playerNum.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigid.velocity = new Vector2 (Input.GetAxis (horAxis) * walkSpeed, Input.GetAxis (vertAxis) * walkSpeed);
+		//TODO : Add dashing in
+	}
+}
