2802e303879abd37231a0d1819d51a07580941df
Oakes <boakes1@trinity.local>
2016-11-01 16:21:58 -0500

    Added Output of the compared Lists

--- Assets/Scripts/MemoryDisplay.cs
+++ Assets/Scripts/MemoryDisplay.cs
@@ -7,20 +7,32 @@ public enum Buttons { A, B, X, Y }; // buttons that can be recognized
 public class MemoryDisplay : MonoBehaviour {
 
 	
-	List<Buttons> InputList = new List<Buttons>(); // list of inputs that will be memorized by player *remember to clear after every round
-	public int Round = 0; // records how many 'rounds' have occured during the game
+	public List<Buttons> InputList = new List<Buttons>(); // list of inputs that will be memorized by player *remember to clear after every round
+	public List<Buttons> PlayerList = new List<Buttons>();
+	public List<int> ReturnList = new List<int>();
+	public int Round = 5; // records how many 'rounds' have occured during the game
     float displayTime = 2f;
+	public GameObject player1 = null;
 
     public Text RandomInputText;
+	public Text OutputText;
+
     int x = 0; // temp display number
 	// Use this for initialization
 	void Start () {
         getRandomInputs();
+		player1 = GameObject.FindGameObjectWithTag ("Player1");
     }
 	
 	// Update is called once per frame
 	void Update () {
         RandomInputText = GetComponent<Text>();
+		OutputText = GameObject.Find ("PlayerText").GetComponent<Text>();
+		PlayerList = player1.GetComponent<PlayerScript> ().InputList;
+		if (PlayerList.Count >= Round) {
+			ReturnList = compareInputs (PlayerList, InputList);
+		}
+		printOutputs (ReturnList);
         //StartCoroutine(timeToDisplay(x.ToString(), 2f));
     }
 
@@ -33,6 +45,17 @@ public class MemoryDisplay : MonoBehaviour {
         //RandomInputText.enabled = false;
         x += 1;
     }
+
+	void printOutputs(List<int> PassedList){
+		string message = "";
+
+		for (int i = 0; i < PassedList.Count; i++) {
+			message += PassedList [i] + " "; 
+		}
+
+		OutputText.text = message;
+		OutputText.enabled = true;
+	}
 
     //gets inputs from generateRandomInputs
     //compares inputs to create message and call timeToDisplay
