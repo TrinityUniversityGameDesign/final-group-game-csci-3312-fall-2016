021a66eebad67ec844f1ae0fc226ee4bfc5174dc
MiguelAReyes <mreyes1@trinity.local>
2016-11-01 14:54:04 -0500

    added flash script
    Made a flash script that takes in all the rooms and makes them dissapear
    and only when the flash input is hit(space bar) will it show the rooms
    for 0.2 seconds. Only enemies should have this input right???

---
+++ Assets/darkroom_Assets/Scripts/flash.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flash : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject[] rooms;
+	void Start () {
+        foreach (GameObject room in rooms)
+        {
+            room.SetActive(false);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float flash = Input.GetAxisRaw("Flash");
+        if(flash > 0)
+        {
+            Debug.Log("space hit");
+            FlashMap();
+        }
+
+    }
+
+    private void FlashMap()
+    {
+        foreach (GameObject room in rooms)
+        {
+            room.SetActive(true);
+        }
+        StartCoroutine(ExecuteAfterTime(0.2f));  
+
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+       foreach(GameObject room in rooms)
+        {
+            room.SetActive(false);
+        }
+    }
+}
