e9e41007dfdb1c7bc6dceb89bf213fca87beaddb
ceckman <cwe118@gmail.com>
2016-11-02 12:58:05 -0500

    Somebody once told me the world is gonna roll me
    added UI and end game states, as well as cleaning up the game in general

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+    private Text winText;
+    private Text timer;
+
+    public float time = 60f;
+
+    public static int alivePlayers = 2;
+
+	// Use this for initialization
+	void Start () {
+        winText = GameObject.Find("WinText").GetComponent<Text>();
+        winText.gameObject.SetActive(false);
+        timer = GameObject.Find("Timer").GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (alivePlayers == 1)
+        {
+            winText.gameObject.SetActive(true);
+            string winPlayer = GameObject.FindGameObjectWithTag("Player").name;
+            winText.text = winPlayer + " wins!";
+        }
+        else if(time <= 0)
+        {
+            winText.gameObject.SetActive(true);
+            timer.text = "0";
+            winText.text = "Game Over";
+        }
+        else
+        {
+            time -= Time.deltaTime;
+            timer.text = time.ToString("n0");
+        }
+	}
+}
