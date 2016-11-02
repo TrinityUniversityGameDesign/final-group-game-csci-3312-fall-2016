a5e2491228b9b14d6da193cf93e08d6b67959f42
chayes4 <chayes@trinity.edu>
2016-11-02 18:28:08 -0500

    Working Prototype in UsingTiles Scene

--- Assets/Scripts/TrivaController.cs
+++ Assets/Scripts/TrivaController.cs
@@ -3,12 +3,14 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class TrivaController : MonoBehaviour {
+	public Color Highest;
+	public Color Lowest;
 	public Text txtScore;
 	int question = 1;
 	int score = 1000;
 	public GameObject controller;
 	public Image scoreBar;
-	private int readTime = 200;
+	private int readTime = 150;
 
 	// Use this for initialization
 	void Start () {
@@ -24,6 +26,7 @@ public class TrivaController : MonoBehaviour {
 		else if (readTime > 0)
 			readTime--;
 		scoreBar.fillAmount = (score / 1000f);
+		txtScore.color = Color.Lerp (Lowest, Highest, scoreBar.fillAmount);
 	}
 	public int amIRight(char answer){
 		if (question == 1) {
@@ -34,5 +37,10 @@ public class TrivaController : MonoBehaviour {
 		} else
 			return 0;
 	}
-
+	public bool canIAnswer(){
+		if (readTime == 0 && score > 0)
+			return true;
+		else
+			return false;
+	}
 }
--- Assets/Scripts/TriviaPlayer.cs
+++ Assets/Scripts/TriviaPlayer.cs
@@ -23,20 +23,20 @@ public class TriviaPlayer : MonoBehaviour
 	void Update ()
 	{
 		txtScore.text = "Score : " + score;
-		if (!answered) {
+		if (!answered && controller.canIAnswer()) {
 			
-			if (Input.GetAxis ("A_P" + 1) > 0) {
+			if (Input.GetAxis ("A_P" + playerNumber) > 0) {
 				answered = true;
 				score += controller.amIRight ('a');
-			} else if (Input.GetAxis ("X_P" + 1) > 0) {
+			} else if (Input.GetAxis ("X_P" + playerNumber) > 0) {
 				answered = true;
 				score += controller.amIRight ('x');
 			}
-			else if (Input.GetAxis ("B_P" + 1) > 0) {
+			else if (Input.GetAxis ("B_P" + playerNumber) > 0) {
 				answered = true;
 				score += controller.amIRight ('b');
 			}
-			else if (Input.GetAxis ("Y_P" + 1) > 0) {
+			else if (Input.GetAxis ("Y_P" + playerNumber) > 0) {
 				answered = true;
 				score += controller.amIRight ('y');
 			}
