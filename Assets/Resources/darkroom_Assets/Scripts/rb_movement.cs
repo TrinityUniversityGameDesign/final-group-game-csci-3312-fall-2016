7344f1ddee0f2fde299c70e29a1cb6717e197aa6
Subrat Mahapatra <Dedalus@dedalus.trinity.local>
2016-11-02 17:23:23 -0500

    test_move

---
+++ Assets/Resources/darkroom_Assets/Scripts/rb_movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rb_movement : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public GameObject player;
+	public float speed = .05f;
+	Vector3 player_pos;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		player_pos = player.transform.position;  
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, player_pos.z);
+		rb.AddForce (movement * speed);
+	}
+}
