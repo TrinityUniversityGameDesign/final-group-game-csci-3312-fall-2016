0dd351c569a3ae6178aea4b9e57a2ba651cb9c5b
mmaryyoung <jyang@trinity.local>
2016-11-02 19:13:47 -0500

    Door/door_script

--- Assets/Resources/darkroom_Assets/Scripts/door.cs
+++ Assets/Resources/darkroom_Assets/Scripts/door.cs
@@ -2,14 +2,52 @@
 using System.Collections;
 
 public class door : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public bool opening;
+    public bool closing;
+    public bool locked;
+    public GameObject dr;
+    public GameObject player;
+    public int numPlayers;
+    public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject enemy3;
+    public float slideDistance;
+    public float slideSpeed;
+    // Use this for initialization
+    void Start () {
+        this.locked = false;
+        this.opening = false;
+        this.closing = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (opening)
+        {
+            if (dr.transform.localPosition.x < slideDistance)
+            {
+                dr.transform.localPosition = new Vector3(dr.transform.localPosition.x + Time.deltaTime * slideSpeed, dr.transform.localPosition.y, dr.transform.localPosition.z);
+            }
+            else
+            {
+                opening = false;
+            }
+        }
+        if (closing)
+        {
+            if (dr.transform.localPosition.x > 0)
+            {
+                dr.transform.localPosition = new Vector3(dr.transform.localPosition.x - Time.deltaTime * slideSpeed, dr.transform.localPosition.y, dr.transform.localPosition.z);
+            }
+            else
+            {
+                closing = false;
+            }
+        }
+    }
+
+    public void LockDoor()
+    {
+        locked = true;
+    }
 }
---
+++ Assets/Resources/darkroom_Assets/Scripts/door_script.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class door_script : MonoBehaviour {
+    public GameObject player;
+    public int numPlayers;
+    public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject enemy3;
+
+    void Start()
+    {
+        player = this.transform.parent.GetComponent<door>().player;
+        enemy1 = this.transform.parent.GetComponent<door>().enemy1;
+        enemy2 = this.transform.parent.GetComponent<door>().enemy2;
+        enemy3 = this.transform.parent.GetComponent<door>().enemy3;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == player || other.gameObject == enemy1 || other.gameObject == enemy2 || other.gameObject == enemy3)
+        {
+            if (this.transform.parent.GetComponent<door>().locked == false)
+            {
+                this.transform.parent.GetComponent<door>().opening = true;
+            }
+        }
+        else
+        {
+            this.transform.parent.GetComponent<door>().closing = true;
+        }
+    }
+}
