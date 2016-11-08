5b1310f41e372bc8a0730ddcbcbd587e5dd7c2de
craig-burton <cburton@trinity.edu>
2016-11-08 15:40:56 -0600

    Named player and script differently

--- Assets/Scripts/PlayerMovement_Jacket.cs
+++ Assets/Scripts/PlayerMovement_Jacket.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement_Jacket : MonoBehaviour {
 
 	public int playerNum;
 	public float walkSpeed;
