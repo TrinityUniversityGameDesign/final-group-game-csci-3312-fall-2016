9ada0e5c0431ad2b786e710e4f565292005ee17d
Emily Herbert <eherbert@trinity.edu>
2016-11-08 12:23:54 -0600

    adding colliders to edge prefabs and changing terraingenerator script

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -54,6 +54,7 @@ public class TerrainGenerator : MonoBehaviour {
         //int x = UnityEngine.Random.Range(0, (int)(width * 0.6) + (int)(width * 0.2));
         board[width/2,height/2] = 1;
         for (int i = 0; i < depth; i++) board = fillTerrain((int[,])board.Clone());
+        checkForEdges();
         checkForBoundaries();
     }
 
@@ -89,7 +90,7 @@ public class TerrainGenerator : MonoBehaviour {
         return b;
     }
 
-    void checkForBoundaries()
+    void checkForEdges()
     {
         for(int i=0; i<width; i++)
         {
@@ -102,8 +103,21 @@ public class TerrainGenerator : MonoBehaviour {
                     if ((j - 1) > -1 && board[i, j - 1] == 0) res = true;
                     if ((j + 1) < height && board[i, j + 1] == 0) res = true;
                     if ((i + 1) < width && board[i + 1, j] == 0) res = true;
+                    if ((i == 0)) res = true;
+                    else if ((i == (width - 1))) res = true;
                 }
                 if (res == true) board[i, j] = 2;
+            }
+        }
+    }
+
+    void checkForBoundaries()
+    {
+        for(int i=0; i<width; i++)
+        {
+            for(int j=0; j<height; j++)
+            {
+                if((board[i,j]==1 || board[i,j]==2) && ((i==0 || i==(width-1)) || (j==0 || j==(height-1)))) { board[i, j] = 3; }
             }
         }
     }
@@ -114,59 +128,102 @@ public class TerrainGenerator : MonoBehaviour {
         {
             for(int j=0; j<height; j++)
             {
-                if(board[i,j]!=0) createSprite(i, j);
+                if (board[i, j] == 2) createEdgeSprite(i, j);
+                else if (board[i, j] == 1) createTerrainSprite(i, j);
+                else if (board[i, j] == 3) createBoundarySprite(i, j);
             }
         }
     }
 
-    void createSprite(int i, int j)
+    void createEdgeSprite(int i, int j)
     {
         GameObject image = one;
         try
         {
-            if (board[i, j] == 2)
-            {
-                /*
-                if (board[i, j - 1] == 0 && board[i - 1, j] != 0 && board[i, j + 1] != 0 && board[i + 1, j] != 0) image = W;
-                else if (board[i, j - 1] != 0 && board[i - 1, j] == 0 && board[i, j + 1] != 0 && board[i + 1, j] != 0) image = N;
-                else if (board[i, j - 1] != 0 && board[i - 1, j] != 0 && board[i, j + 1] == 0 && board[i + 1, j] != 0) image = E;
-                else if (board[i, j - 1] != 0 && board[i - 1, j] != 0 && board[i, j + 1] != 0 && board[i + 1, j] == 0) image = S;
-                else if (board[i, j - 1] == 0 && board[i - 1, j] == 0 && board[i, j + 1] != 0 && board[i + 1, j] != 0) image = WN;
-                else if (board[i, j - 1] == 0 && board[i - 1, j] != 0 && board[i, j + 1] == 0 && board[i + 1, j] != 0) image = WE;
-                else if (board[i, j - 1] == 0 && board[i - 1, j] != 0 && board[i, j + 1] != 0 && board[i + 1, j] == 0) image = WS;
-                else if (board[i, j - 1] != 0 && board[i - 1, j] == 0 && board[i, j + 1] == 0 && board[i + 1, j] != 0) image = NE;
-                else if (board[i, j - 1] != 0 && board[i - 1, j] == 0 && board[i, j + 1] != 0 && board[i + 1, j] == 0) image = NS;
-                else if (board[i, j - 1] != 0 && board[i - 1, j] != 0 && board[i, j + 1] == 0 && board[i + 1, j] == 0) image = ES;
-                else if (board[i, j - 1] == 0 && board[i - 1, j] == 0 && board[i, j + 1] == 0 && board[i + 1, j] != 0) image = WNE;
-                else if (board[i, j - 1] == 0 && board[i - 1, j] == 0 && board[i, j + 1] != 0 && board[i + 1, j] == 0) image = WNS;
-                else if (board[i, j - 1] == 0 && board[i - 1, j] != 0 && board[i, j + 1] == 0 && board[i + 1, j] == 0) image = WES;
-                else if (board[i, j - 1] != 0 && board[i - 1, j] == 0 && board[i, j + 1] == 0 && board[i + 1, j] == 0) image = NES;
-                else if (board[i, j - 1] == 0 && board[i - 1, j] == 0 && board[i, j + 1] == 0 && board[i + 1, j] == 0) image = WNES;
-                */
                 if (board[i - 1, j] == 0 && board[i, j - 1] != 0 && board[i + 1, j] != 0 && board[i, j + 1] != 0) image = W;
-                else if (board[i - 1, j] != 0 && board[i, j - 1] == 0 && board[i + 1, j] != 0 && board[i, j + 1] != 0) image = N;
+                else if (board[i - 1, j] != 0 && board[i, j - 1] == 0 && board[i + 1, j] != 0 && board[i, j + 1] != 0) image = S;
                 else if (board[i - 1, j] != 0 && board[i, j - 1] != 0 && board[i + 1, j] == 0 && board[i, j + 1] != 0) image = E;
-                else if (board[i - 1, j] != 0 && board[i, j - 1] != 0 && board[i + 1, j] != 0 && board[i, j + 1] == 0) image = S;
-                else if (board[i - 1, j] == 0 && board[i, j - 1] == 0 && board[i + 1, j] != 0 && board[i, j + 1] != 0) image = WN;
+                else if (board[i - 1, j] != 0 && board[i, j - 1] != 0 && board[i + 1, j] != 0 && board[i, j + 1] == 0) image = N;
+                else if (board[i - 1, j] == 0 && board[i, j - 1] == 0 && board[i + 1, j] != 0 && board[i, j + 1] != 0) image = WS;
                 else if (board[i - 1, j] == 0 && board[i, j - 1] != 0 && board[i + 1, j] == 0 && board[i, j + 1] != 0) image = WE;
-                else if (board[i - 1, j] == 0 && board[i, j - 1] != 0 && board[i + 1, j] != 0 && board[i, j + 1] == 0) image = WS;
-                else if (board[i - 1, j] != 0 && board[i, j - 1] == 0 && board[i + 1, j] == 0 && board[i, j + 1] != 0) image = NE;
+                else if (board[i - 1, j] == 0 && board[i, j - 1] != 0 && board[i + 1, j] != 0 && board[i, j + 1] == 0) image = WN;
+                else if (board[i - 1, j] != 0 && board[i, j - 1] == 0 && board[i + 1, j] == 0 && board[i, j + 1] != 0) image = ES;
                 else if (board[i - 1, j] != 0 && board[i, j - 1] == 0 && board[i + 1, j] != 0 && board[i, j + 1] == 0) image = NS;
-                else if (board[i - 1, j] != 0 && board[i, j - 1] != 0 && board[i + 1, j] == 0 && board[i, j + 1] == 0) image = ES;
-                else if (board[i - 1, j] == 0 && board[i, j - 1] == 0 && board[i + 1, j] == 0 && board[i, j + 1] != 0) image = WNE;
+                else if (board[i - 1, j] != 0 && board[i, j - 1] != 0 && board[i + 1, j] == 0 && board[i, j + 1] == 0) image = NE;
+                else if (board[i - 1, j] == 0 && board[i, j - 1] == 0 && board[i + 1, j] == 0 && board[i, j + 1] != 0) image = WES;
                 else if (board[i - 1, j] == 0 && board[i, j - 1] == 0 && board[i + 1, j] != 0 && board[i, j + 1] == 0) image = WNS;
-                else if (board[i - 1, j] == 0 && board[i, j - 1] != 0 && board[i + 1, j] == 0 && board[i, j + 1] == 0) image = WES;
+                else if (board[i - 1, j] == 0 && board[i, j - 1] != 0 && board[i + 1, j] == 0 && board[i, j + 1] == 0) image = WNE;
                 else if (board[i - 1, j] != 0 && board[i, j - 1] == 0 && board[i + 1, j] == 0 && board[i, j + 1] == 0) image = NES;
                 else if (board[i - 1, j] == 0 && board[i, j - 1] == 0 && board[i + 1, j] == 0 && board[i, j + 1] == 0) image = WNES;
-            }
-            else
+        }
+        catch (Exception e)
+        {
+
+        }
+
+        float x = Math.Abs((width / 2) - i) * 0.74f;
+        if (i < (width / 2)) x *= (-1);
+        float y = Math.Abs((height / 2) - j) * 0.74f;
+        if (j < (height / 2)) y *= (-1);
+
+        GameObject tmpTerrain = Instantiate(image, new Vector3(x, y, this.transform.localPosition.z), Quaternion.identity) as GameObject;
+        tmpTerrain.transform.parent = gameObject.transform;
+    }
+
+    void createBoundarySprite(int i, int j)
+    {
+        GameObject image = one;
+        try
+        {
+            Debug.Log(j);
+            if(j==(height-1))
             {
+                Debug.Log("check");
+                if (board[i, j - 1] == 0 && board[i, j + 1] == 0 && board[i - 1, j] == 0) image = WNES;
+                else if (board[i, j - 1] == 0 && board[i, j + 1] == 0 && board[i - 1, j] != 0) image = WES;
+                else if (board[i, j - 1] == 0 && board[i, j + 1] != 0 && board[i - 1, j] == 0) image = WNS;
+                else if (board[i, j - 1] != 0 && board[i, j + 1] == 0 && board[i - 1, j] == 0) image = NES;
+                else if (board[i, j - 1] != 0 && board[i, j + 1] == 0 && board[i - 1, j] != 0) image = ES;
+                else if (board[i, j - 1] == 0 && board[i, j + 1] != 0 && board[i - 1, j] != 0) image = WS;
+                else if (board[i, j - 1] != 0 && board[i, j + 1] != 0 && board[i - 1, j] == 0) image = NS;
+                else image = S;
+                Debug.Log(image);
+            } else if (j==0)
+            {
+                if (board[i, j - 1] == 0 && board[i, j + 1] == 0 && board[i + 1, j] == 0) image = WNES;
+                else if (board[i, j - 1] == 0 && board[i, j + 1] == 0 && board[i + 1, j] != 0) image = WNE;
+                else if (board[i, j - 1] == 0 && board[i, j + 1] != 0 && board[i + 1, j] == 0) image = WNS;
+                else if (board[i, j - 1] != 0 && board[i, j + 1] == 0 && board[i + 1, j] == 0) image = NES;
+                else if (board[i, j - 1] != 0 && board[i, j + 1] == 0 && board[i + 1, j] != 0) image = NE;
+                else if (board[i, j - 1] == 0 && board[i, j + 1] != 0 && board[i + 1, j] != 0) image = WN;
+                else if (board[i, j - 1] != 0 && board[i, j + 1] != 0 && board[i + 1, j] == 0) image = NS;
+                else image = N;
+            }
+        }
+        catch (Exception e)
+        {
+
+        }
+
+        float x = Math.Abs((width / 2) - i) * 0.74f;
+        if (i < (width / 2)) x *= (-1);
+        float y = Math.Abs((height / 2) - j) * 0.74f;
+        if (j < (height / 2)) y *= (-1);
+
+        GameObject tmpTerrain = Instantiate(image, new Vector3(x, y, this.transform.localPosition.z), Quaternion.identity) as GameObject;
+        tmpTerrain.transform.parent = gameObject.transform;
+    }
+
+    void createTerrainSprite(int i, int j)
+    {
+        GameObject image = one;
+        try
+        {
                 int rand = UnityEngine.Random.Range(0, 4);
                 if (rand == 0) image = zero;
                 else if (rand == 1) image = one;
                 else if (rand == 2) image = two;
                 else image = three;
-            }
         } catch (Exception e)
         {
 
