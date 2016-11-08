67ac59413af72863b64c273b9e61c1228e5889e5
mmaryyoung <jyang@trinity.local>
2016-11-08 16:29:37 -0600

    random key generation

---
+++ Assets/Resources/darkroom_Assets/Scripts/keyGeneration.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class keyGeneration : MonoBehaviour {
+    private Vector3[] keyLocs;
+    private GameObject key;
+    private ArrayList keys;
+    public int keyNum = 6;
+
+    // Use this for initialization
+    void Start()
+    {
+        keyLocs = new[] { new Vector3(-.67f, 5.39f, 0f),
+                          new Vector3(-8f, 2.5f, 0f),
+                          new Vector3(0.7f, 2.45f, 0f),
+                          new Vector3(0.5f, 2.45f, 0f),
+                          new Vector3(12.6f, 6f, 0f),
+                          new Vector3(17f, 6f, 0f),
+                          new Vector3(17f, -3.15f, 0f),
+                          new Vector3(7.4f, -6.9f, 0f),
+                          new Vector3(2f, -2.3f, 0f)
+       };
+        key = Resources.Load<GameObject>("darkroom_Assets/Resources/Prefabs/key");
+        HashSet<int> keypool = new HashSet<int>();
+        keys = new ArrayList();
+        int rover = keyNum;
+        while (rover > 0)
+        {
+            bool success = false;
+            while (!success)
+            {
+                int pos = (int)Random.Range(0, keyLocs.Length);
+                success = keypool.Add(pos);
+            }
+            --rover;
+        }
+        Debug.Log(keypool.Count);
+        foreach (int i in keypool)
+        {
+            GameObject newkey = (GameObject)Instantiate(key, keyLocs[i], Quaternion.identity);
+            newkey.transform.parent = gameObject.transform;
+            newkey.transform.localPosition = keyLocs[i];
+            keys.Add(newkey);
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
