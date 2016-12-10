07400abc4d42c3fa145d656c8e63610ac91f5fe2
boakes <boakes1@trinity.local>
2016-12-09 22:23:15 -0600

    GameControllerScene
    Made most of the character select screen. Still need to debug a bit and
    make it look a tad more pretty.

---
+++ Assets/Scripts/ColorPickerScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ColorPickerScript : MonoBehaviour {
+
+	int red = 0;
+	int green = 0;
+	int blue = 0;
+	int alpha;
+
+	public Slider redSlider;
+	public Slider blueSlider;
+	public Slider greenSlider;
+
+	public GameObject shirt;
+	public GameObject player;
+
+	public Color chc; 
+	// Use this for initialization
+	void Start () {
+		alpha = 1;
+		chc.a = alpha;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (player != null) {
+			player.GetComponent<SelectPlayerControls> ().colorPicker = this;
+			shirt = player.transform.Find ("Shirt").gameObject;
+		}
+
+		if (shirt != null) {
+			shirt.GetComponent<SpriteRenderer> ().color = chc;
+		}
+
+		if (redSlider != null) {
+			chc.r = redSlider.value;
+		}
+
+		if (blueSlider != null) {
+			chc.b = blueSlider.value;
+		}
+
+		if (greenSlider != null) {
+			chc.g = greenSlider.value;
+		}
+		
+		
+
+	}
+}
---
+++ Assets/Scripts/MySlider.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MySlider : Slider{
+	public EventSystem eventSystem;
+
+	protected override void Awake(){
+		base.Awake ();
+		if (this.transform.parent.name == "Player1Color") {
+			eventSystem = GameObject.Find("EventSystemProvider").GetComponent<EventSystemProvider> ().eventSystem_p1;
+		}
+		if (this.transform.parent.name == "Player2Color") {
+			eventSystem = GameObject.Find("EventSystemProvider").GetComponent<EventSystemProvider> ().eventSystem_p2;
+		}
+		if (this.transform.parent.name == "Player3Color") {
+			eventSystem = GameObject.Find("EventSystemProvider").GetComponent<EventSystemProvider> ().eventSystem_p3;
+		}
+		if (this.transform.parent.name == "Player4Color") {
+			eventSystem = GameObject.Find("EventSystemProvider").GetComponent<EventSystemProvider> ().eventSystem_p4;
+		}
+	}
+
+	public override void Select(){
+
+		if (eventSystem.alreadySelecting) {
+			return;
+		}
+
+		eventSystem.SetSelectedGameObject (gameObject);
+	}
+
+}
