756a8297849a0fbfc43a7bdc874edd3608a350f4
astralsquid <Brewmaster@brewmaster.trinity.local>
2016-12-10 18:37:54 -0600

    Title Scene

---
+++ Assets/IntroControllerScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroControllerScript : MonoBehaviour {
+	GameObject SmackText;
+	GameObject BowlMec;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
