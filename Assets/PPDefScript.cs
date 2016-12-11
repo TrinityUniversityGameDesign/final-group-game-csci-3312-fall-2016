dd687bfa9f5f7533c0128fe7373b59672a76f953
astralsquid <Brewmaster@brewmaster.trinity.local>
2016-12-11 13:36:29 -0600

    PlayerPrefValues

--- Assets/PPDefScript.cs
+++ Assets/PPDefScript.cs
@@ -4,7 +4,20 @@ using System.Collections;
 public class PPDefScript : MonoBehaviour {
 
 	void Awake(){
+		PlayerPrefs.SetInt ("player1_score", 1);
+		PlayerPrefs.SetInt ("player2_score", 2);
+		PlayerPrefs.SetInt ("player3_score", 3);
+		PlayerPrefs.SetInt ("player4_score", 4);
 
+		PlayerPrefs.SetString ("player1_name", "Beano");
+		PlayerPrefs.SetString ("player2_name", "Cheddar");
+		PlayerPrefs.SetString ("player3_name", "Nad");
+		PlayerPrefs.SetString ("player4_name", "Bonus Jonas");
+
+		PlayerPrefs.SetString ("player1_color", "#4286f4");
+		PlayerPrefs.SetString ("player2_color", "#7af442");
+		PlayerPrefs.SetString ("player3_color", "#af2845");
+		PlayerPrefs.SetString ("player3_color", "#d8d50f");
 	}
 
 	// Use this for initialization
