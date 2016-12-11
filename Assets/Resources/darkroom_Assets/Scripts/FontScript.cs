8d144ae932c0890aa7bd341949275cabdc186972
MiguelAReyes <mreyes1@trinity.local>
2016-12-11 12:59:24 -0600

    fonts and ending

---
+++ Assets/Resources/darkroom_Assets/Scripts/FontScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FontScript : MonoBehaviour {
+
+    // Use this for initialization
+    
+    public GameObject[] texts;
+    string font = PlayerPrefs.GetString("font_name");
+    void Start () {
+       
+        //SetFont(font);
+	}
+    void SetFont(string f)
+    {
+        foreach(GameObject x in texts)
+        {
+            x.GetComponent<Text>().font = Resources.GetBuiltinResource<Font>(f + ".ttf");
+        }
+        
+       
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
