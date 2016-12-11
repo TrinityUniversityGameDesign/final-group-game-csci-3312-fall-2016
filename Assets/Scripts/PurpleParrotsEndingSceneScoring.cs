a6c72174d641d800155c44e3f83f4e6ce1572b94
Marcus Whitten <cwhitte1@trinity.edu>
2016-12-11 12:36:08 -0600

    ending scene progress

--- Assets/Scripts/PurpleParrotsEndingSceneScoring.cs
+++ Assets/Scripts/PurpleParrotsEndingSceneScoring.cs
@@ -24,6 +24,11 @@ public class PurpleParrotsEndingSceneScoring : MonoBehaviour {
 		player3_rank = sneakyFinds.GetComponent<SneakyScript>().p3Rank;
 		player4_rank = sneakyFinds.GetComponent<SneakyScript>().p4Rank;
 
+		player1_rank = 1;
+		player2_rank = 4;
+		player3_rank = 2;
+		player4_rank = 2;
+
 		int p1 = PlayerPrefs.GetInt("player1_score");
 		int p2 = PlayerPrefs.GetInt("player2_score");
 		int p3 = PlayerPrefs.GetInt("player3_score");
@@ -35,35 +40,24 @@ public class PurpleParrotsEndingSceneScoring : MonoBehaviour {
 			p1 += 3;
 		} else if (player1_rank == 3) {
 			p1 += 1;
+		} else {
+			p1 += 0;
 		}
-
 		/*
-		if (player1_rank != -1) {
-			GameObject column1 = GameObject.Find ("Column1");
-			column1.transform.position = new Vector3 (column1.transform.position.x, column1.transform.position.y + 1, column1.transform.position.z);
-		}
-		if (player2_rank != -1) {
-			GameObject column2 = GameObject.Find ("Column2");
-			column2.transform.position = new Vector3 (column2.transform.position.x, column2.transform.position.y + 1, column2.transform.position.z);
-		}
-		if (player3_rank != -1) {
-			GameObject column3 = GameObject.Find ("Column3");
-			column3.transform.position = new Vector3 (column3.transform.position.x, column3.transform.position.y + 1, column3.transform.position.z);
-		}
-		if (player4_rank != -1) {
-			GameObject column4 = GameObject.Find ("Column4");
-			column4.transform.position = new Vector3 (column4.transform.position.x, column4.transform.position.y + 1, column4.transform.position.z);
-		}
-		*/
-
-		column1_adj = -3.5f - player1_rank + 1f; 
+		column1_adj = -3.5f - player1_rank + 1f;
 		column2_adj = -3.5f - player2_rank + 1f;
 		column3_adj = -3.5f - player3_rank + 1f;
 		column4_adj = -3.5f - player4_rank + 1f;
+		*/
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		column1_adj = -3.5f - player1_rank + 1f;
+		column2_adj = -3.5f - player2_rank + 1f;
+		column3_adj = -3.5f - player3_rank + 1f;
+		column4_adj = -3.5f - player4_rank + 1f;
+
 		GameObject column1 = GameObject.Find ("Column1");
 		GameObject column2 = GameObject.Find ("Column2");
 		GameObject column3 = GameObject.Find ("Column3");
@@ -78,7 +72,7 @@ public class PurpleParrotsEndingSceneScoring : MonoBehaviour {
 			//column2_adj += .05f;
 		}
 		if (column3.transform.position.y <= column3_adj && (player3_rank != -1)) {
-			
+
 			column3.transform.position = new Vector3(column3.transform.position.x, column3.transform.position.y + .05f, column3.transform.position.z);
 			//column3_adj += .05f;
 		}
