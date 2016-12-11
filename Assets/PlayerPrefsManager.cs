0cad6c92cec1dfb7699a1a540c6a1c507e3a6bbb
Emily Herbert <eherbert@trinity.edu>
2016-12-11 03:45:05 -0600

    working on player pref implementation

---
+++ Assets/PlayerPrefsManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerPrefsManager : MonoBehaviour {
+
+    public GameObject playerOne;
+    public GameObject playerTwo;
+    public GameObject playerThree;
+    public GameObject playerFour;
+
+    private int numPlayers;
+    private List<string> playerNames;
+    private List<int> playerScores;
+    private List<string> playerColors;
+    private List<GameObject> playerObjects;
+
+    void Awake()
+    {
+        playerObjects = new List<GameObject> { playerOne, playerTwo, playerThree, playerFour };
+    }
+    
+	// Use this for initialization
+	void Start () {
+        for(int i=0; i<numPlayers; i++) {
+            playerObjects[i].SetActive(true);
+        }
+	    for(int i=1; i<(numPlayers+1); i++) {
+            playerNames.Add(PlayerPrefs.GetString("player" + i + "_score"));
+            playerScores.Add(PlayerPrefs.GetInt("player" + i + "_score"));
+            playerColors.Add(PlayerPrefs.GetString("player" + i + "_color"));
+        }
+        for(int i=0; i<numPlayers; i++) {
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
